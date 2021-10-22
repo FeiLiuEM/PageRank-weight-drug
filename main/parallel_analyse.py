684bb32c3a5f99052bbe3c84d69f6fcbcf6a2c13
FeiLiuEM <feiliuem@outlook.com>
2021-10-22 10:59:29 +0800

    a little update

--- main/parallel_analyse.py
+++ main/parallel_analyse.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-motrix=pd.read_csv("../data/motrix.csv")
+motrix=pd.read_csv("~/SYNC/CODE/ML-AI/Ranking/pagerank-weight/data/motrix.csv")
 
 motrix.head()
 
@@ -11,4 +11,18 @@ protein1=motrix["protein"].values.tolist()
 drug1=motrix["drug"].values.tolist()
 
 len_motrix=len(protein1)
+
+value1=list=[5 for x in range(0,len_motrix)]
+
+len(value1)
+
+new_pd=pd.DataFrame()
+
+new_pd["protein"]=drug1
+new_pd["drug"]=protein1
+new_pd["value"]=value1
+
+
+
+
 
