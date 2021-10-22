8480cea85838a47745f8006305f752cfe55aa25c
FeiLiuEM <feiliuem@outlook.com>
2021-10-22 23:44:56 +0800

    a little update

--- main/parallel_analyse.py
+++ main/parallel_analyse.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import itertools
 
 motrix=pd.read_csv("~/SYNC/CODE/ML-AI/Ranking/pagerank-weight/data/motrix.csv")
 num_parallel=2 #多少种药物联合
@@ -15,9 +15,6 @@ drug1=motrix["drug"].values.tolist()
 len_motrix=len(protein1)
 
 value1=[5 for x in range(0,len_motrix)]
-
-len(value1)
-
 
 protein=motrix["protein"].values.tolist()+drug1
 drug=motrix["drug"].values.tolist()+protein1
@@ -42,6 +39,15 @@ all_drug = list(set(drug1))
 #进行排序
 all_protein.sort(key=protein1.index)
 all_drug.sort(key=drug1.index)
+#len(all_drug)
+
+aa = ['a', 'b', 'c']
+bb = list(itertools.permutations(aa, 2))
+
+drug_combination=list(itertools.permutations(all_drug, num_parallel))  #生成药物联合治疗的排列组合
+
+len(drug_combination)
+
 len(all_drug)
 
-
+drug_combination[-1]
