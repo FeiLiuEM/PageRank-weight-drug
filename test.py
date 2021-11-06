24db53a3903d05efea0ac120d8791fbf28c8c697
FeiLiuEM <feiliuem@outlook.com>
2021-11-06 22:58:45 +0800

    a little update

--- test.py
+++ test.py
@@ -10,19 +10,29 @@ import networkx as nx
 import pandas as pd
 import numpy as np
 
+from main import xlsx2motrix
+from main import parallel_analyse
 
+#motrix=xlsx2motrix.motrix_generate('./data/DATA.xlsx')
 
-inlinks_csv = './data/pr-demo.csv'
-df = pd.read_csv(inlinks_csv)
-df
+motrix=pd.read_csv('./data/motrix_test.csv')
 
+weight_dict={'A':1, 'B':1, 'C':1,'D':1}
 
+#motrix.to_csv("./data/motrix.csv") 
 
+motrix.columns=['source','target','weight']
+
+df=motrix
 
 G=nx.from_pandas_edgelist(df, 'source', 'target', create_using=nx.DiGraph)
 
 G_weighted=nx.from_pandas_edgelist(df, 'source', 'target', create_using=nx.DiGraph, edge_attr='weight')
 
+weights = [i * 5 for i in df['weight'].tolist()]
+
+'''
+#可视化部分
 weights = [i * 5 for i in df['weight'].tolist()]
 
 pos = nx.spring_layout(G, k=0.9)
@@ -31,37 +41,15 @@ nx.draw_networkx_nodes(G, pos,node_color='#EF476F', node_size=1000)
 nx.draw_networkx_labels(G, pos, font_size=10, font_weight='bold', font_color='white')
 plt.gca().margins(0.1, 0.1)
 plt.show()
+'''
+
+#设置权重
 
 
+#simple_pagerank = nx.pagerank(G, alpha=0.9)
+#personalized_pagerank = nx.pagerank(G, alpha=0.9, personalization=weight_dict)
+#nstart_pagerank = nx.pagerank(G, alpha=0.9, nstart=weight_dict)
+#weighted_pagerank = nx.pagerank(G_weighted, alpha=0.9)
+weighted_personalized_pagerank = nx.pagerank(G_weighted, alpha=0.9, personalization=weight_dict,max_iter=10000,tol=1e-7)
 
-
-simple_pagerank = nx.pagerank(G, alpha=0.85)
-personalized_pagerank = nx.pagerank(G, alpha=0.85, personalization={'A': 10011, 'B': 1, 'C': 1, 'D': 1})
-nstart_pagerank = nx.pagerank(G, alpha=0.85, nstart={'A': 10011, 'B': 1, 'C': 1, 'D': 1})
-weighted_pagerank = nx.pagerank(G_weighted, alpha=0.85)
-weighted_personalized_pagerank = nx.pagerank(G_weighted, alpha=0.85, personalization={'A': 10011, 'B': 1, 'C': 1, 'D': 1})
-
-df_metrics = pd.DataFrame(dict(
-    simple_pagerank = simple_pagerank,
-    personalized_pagerank = personalized_pagerank,
-    nstart_pagerank = nstart_pagerank,
-    weighted_pagerank = weighted_pagerank,
-    weighted_personalized_pagerank = weighted_personalized_pagerank,
-))
-df_metrics.index.name='urls'
-df_metrics
-
-
-
-node_size = [i * 4000 for i in df_metrics['weighted_personalized_pagerank'].to_list()]
-node_size
-
-
-
-weights = [i * 5 for i in df['weight'].tolist()]
-nx.draw_networkx_edges(G_weighted, pos, edge_color='#06D6A0', arrowsize=22, width=weights)
-nx.draw_networkx_nodes(G_weighted, pos,node_color='#EF476F', node_size=node_size)
-nx.draw_networkx_labels(G_weighted, pos, font_size=10, font_weight='bold', font_color='white')
-plt.gca().margins(0.1, 0.1)
-plt.show()
-
+print(weighted_personalized_pagerank)
